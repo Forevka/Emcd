83b3b90ee01f6de69b8a80bb8dfb0b0004d9518e
Forevka <32968153+Forevka@users.noreply.github.com>
2021-03-01 10:58:57 +0000

    fixed

--- handlers/text/locales_command.py
+++ handlers/text/locales_command.py
@@ -1,10 +1,10 @@
 from utils import load_translations
 from aiogram import types
 from database.user_repo import UserRepository
-
+from config import POEDITOR_ID, POEDITOR_TOKEN
 
 async def cmd_locales(message: types.Message, user: UserRepository, _: dict):
-    from config import update_texts, POEDITOR_ID, POEDITOR_TOKEN
+    from lang import update_texts
 
     new_msg =await message.answer("Reloading locales")
 
