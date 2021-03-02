1e51e3f1f390bd1b2fc64922849a47958fcdb2d5
Forevka <32968153+Forevka@users.noreply.github.com>
2021-03-02 21:08:26 +0000

    migrations update

---
+++ migrations/starter_data.sql
@@ -0,0 +1,14 @@
+
+-- ----------------------------
+-- Records of lang
+-- ----------------------------
+INSERT INTO "public"."lang" VALUES (1, 'Русский');
+INSERT INTO "public"."lang" VALUES (2, 'English');
+INSERT INTO "public"."lang" VALUES (3, 'Persian');
+
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO "public"."role" VALUES (1, 'User');
+INSERT INTO "public"."role" VALUES (2, 'Admin');
