ed992a0a9e9789cfa32f5b3c729501b26f80c9d2
Forevka <32968153+Forevka@users.noreply.github.com>
2021-03-13 22:09:12 +0000

    parenthesis

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import logging
 from utils.intercept_standart_logger import InterceptStandartHandler
 
 from fastapi import FastAPI
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from loguru import logger
 
@@ -13,7 +14,7 @@ logging.basicConfig(handlers=[InterceptStandartHandler()], level=logging.INFO)
 logger.add("logs/api_{time}.log", rotation="12:00", serialize=True)
 
 middleware = [ 
-    Middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*']))
+    Middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 ]
 
 app = FastAPI(middleware=middleware)
